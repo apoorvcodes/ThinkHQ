0ac4919261be1a7e5d03bbd2720977272ecaa87e
apoorvcodes <tofu.develops@gmail.com>
2022-03-26 16:27:16 +0530

    feat: add env variable

---
+++ components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex pt-6 pl-12 pr-12 pb-6'>
+        <div className='flex flex-row space-x-4 justify-center items-center'>
+
+        <div className='font-bold text-4xl p-2 text-white bg-purple-700 rounded-md'>
+            HQ
+        </div>
+        <div className='font-bold text-4xl  text-purple-700'>
+            ReThink
+        </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
