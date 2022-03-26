3f5c86cbe70961e46bf27a2e1176a675f7c1e4f4
apoorvcodes <tofu.develops@gmail.com>
2022-03-26 19:31:10 +0530

    feat: done mobile navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
+import {FaGithub} from "react-icons/fa"
 const Navbar = () => {
     const [isMobile, setIsMobile] = useState(false)
     useEffect(()=> {
@@ -12,22 +13,23 @@ const Navbar = () => {
      })
     }, [])
   return (
-    <div className='flex pt-6 pl-24 pr-24 pb-6 justify-between items-center'>
+    <div className='flex pt-6  pl-4 pr-4 md:pl-24 md:pr-24 pb-6 justify-between items-center'>
         <div className='flex flex-row space-x-4 justify-center items-center'>
 
-        <div className='font-bold text-3xl p-2 text-white bg-purple-600 rounded-md'>
+        <div className='font-bold text-2xl md:text-3xl p-2 text-white bg-purple-600 rounded-md'>
             HQ
         </div>
-        <div className='font-bold text-4xl  text-purple-600'>
+        <div className='font-bold text-2xl md:text-4xl  text-purple-600'>
             ReThink
         </div>
         
         </div>
-        <div className='flex flex-row space-x-4 items-center'>
-        {isMobile ? <div>YES</div> : <div>no</div>}
-            <div className='text-xl font-bold '>GITHUB</div>
-            <div className='text-xl font-bold text-white bg-purple-600 p-2 '>GET STARTED  </div>
-            </div>
+        {isMobile ? <div className='flex flex-row space-x-4 items-center'><div className='text-lg font-bold '><FaGithub/></div>
+            <div className='text-sm font-bold text-white bg-purple-600 p-1 '>GET STARTED </div> </div>
+            : <div className='flex flex-row space-x-4 items-center'><div className='text-xl font-bold '>GITHUB</div>
+            <div className='text-xl font-bold text-white bg-purple-600 p-2 '>GET STARTED  </div> </div>}
+            
+            
     </div>
   )
 }
