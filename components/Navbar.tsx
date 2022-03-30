52c95e6b90b031d7335c38a056d7cbdb1fc55c84
apoorvcodes <tofu.develops@gmail.com>
2022-03-30 10:03:28 +0530

    feat: code format and lint

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,46 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import {FaGithub} from "react-icons/fa"
+import React from "react";
+import { useState, useEffect } from "react";
+import { FaGithub } from "react-icons/fa";
 const Navbar = () => {
-    const [isMobile, setIsMobile] = useState(false)
-    useEffect(()=> {
-     window.addEventListener("resize", ()=>{
-         if(window.innerWidth > 768) {
-             setIsMobile(false)
-             return
-         }
-         setIsMobile(true)
-     })
-    }, [])
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      if (window.innerWidth > 768) {
+        setIsMobile(false);
+        return;
+      }
+      setIsMobile(true);
+    });
+  }, []);
   return (
-    <div className='flex pt-6   pb-6 justify-between items-center'>
-        <div className='flex flex-row space-x-4 justify-center items-center'>
-
-        <div className='font-bold text-2xl md:text-3xl p-2 text-white bg-prime rounded-md'>
-            HQ
+    <div className="flex pt-6   pb-6 justify-between items-center">
+      <div className="flex flex-row space-x-4 justify-center items-center">
+        <div className="font-bold text-2xl md:text-3xl p-2 text-white bg-prime rounded-md">
+          HQ
         </div>
-        <div className='font-bold text-2xl md:text-4xl  text-prime'>
-            ReThink
+        <div className="font-bold text-2xl md:text-4xl  text-prime">
+          ReThink
         </div>
-        
+      </div>
+      {isMobile ? (
+        <div className="flex flex-row space-x-4 items-center">
+          <div className="text-lg font-bold ">
+            <FaGithub />
+          </div>
+          <div className="text-sm font-bold text-white bg-prime p-1 ">
+            GET STARTED{" "}
+          </div>{" "}
         </div>
-        {isMobile ? <div className='flex flex-row space-x-4 items-center'><div className='text-lg font-bold '><FaGithub/></div>
-            <div className='text-sm font-bold text-white bg-prime p-1 '>GET STARTED </div> </div>
-            : <div className='flex flex-row space-x-4 items-center'><div className='text-xl font-bold '>GITHUB</div>
-            <div className='text-xl font-bold text-white bg-prime p-2 '>GET STARTED  </div> </div>}
-            
-            
+      ) : (
+        <div className="flex flex-row space-x-4 items-center">
+          <div className="text-xl font-bold ">GITHUB</div>
+          <div className="text-xl font-bold text-white bg-prime p-2 ">
+            GET STARTED{" "}
+          </div>{" "}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from 'next'
-import { Hero } from '../components/Hero'
-import Navbar from '../components/Navbar'
+import type { NextPage } from "next";
+import { Hero } from "../components/Hero";
+import Navbar from "../components/Navbar";
 const Home: NextPage = () => {
   return (
-    <div className='pl-4 pr-4 md:pl-24 md:pr-24 bg-moon w-full min-h-screen text-white'>
-     <Navbar/>
-     <Hero/>
+    <div className="pl-4 pr-4 md:pl-24 md:pr-24 bg-moon w-full min-h-screen text-white">
+      <Navbar />
+      <Hero />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
